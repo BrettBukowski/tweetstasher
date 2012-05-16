dced9397b9569ca1434fc8c6c6be4b67bd6da52d
Brett Bukowski <brett.bukowski@gmail.com>
2012-05-16 07:21:24 -0600

    Plugging into passport

--- app.js
+++ app.js
@@ -5,7 +5,29 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , less = require('less-middleware');
+  , less = require('less-middleware')
+  , passport = require('passport')
+  , twitter = require('passport-twitter').Strategy;
+  
+// Passport setup
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+var credentials = require('./config/passport.js');
+passport.use(new twitter({
+    consumerKey: credentials.key,
+    consumerSecret: credentials.secret,
+    callbackURL: ''
+  },
+  function(token, tokenSecret, profile, done) {
+    
+  }
+));
 
 var app = module.exports = express.createServer();
 
@@ -24,7 +46,7 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  app.use(less({ src: __dirname + '/public' }));
+  app.use(less({ src: __dirname + '/public', force: true }));
 });
 
 app.configure('production', function(){
@@ -35,6 +57,11 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), function(req, res) {
+  res.redirect('/');
+});
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
