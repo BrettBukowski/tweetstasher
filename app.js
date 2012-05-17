d9faa54feb31a2634d384c6c336af72d20797a4c
Brett Bukowski <brett.bukowski@gmail.com>
2012-05-16 21:32:29 -0600

    Switch to everyauth

--- app.js
+++ app.js
@@ -6,28 +6,20 @@
 var express = require('express')
   , routes = require('./routes')
   , less = require('less-middleware')
-  , passport = require('passport')
-  , twitter = require('passport-twitter').Strategy;
-  
-// Passport setup
+  , everyauth = require('everyauth');
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
+// Everyauth setup
 
-var credentials = require('./config/passport.js');
-passport.use(new twitter({
-    consumerKey: credentials.key,
-    consumerSecret: credentials.secret,
-    callbackURL: ''
-  },
-  function(token, tokenSecret, profile, done) {
-    
-  }
-));
+var twitterCredentials = require('./config/twitter.js');
+everyauth.twitter
+  .consumerKey(twitterCredentials.key)
+  .consumerSecret(twitterCredentials.secret)
+  .findOrCreateUser(function(session, accessToken, accessTokenSecret, userInfo) {
+    var promise = this.Promise();
+    users.findOrCreate(userInfo, accessToken, accessTokenSecret, promise);
+    return promise;
+  })
+  .redirectPath('/');
 
 var app = module.exports = express.createServer();
 
@@ -42,6 +34,8 @@ app.configure(function(){
   app.use(express.static(__dirname + '/public'));
   app.use(express.cookieParser());
   app.use(express.session({ secret: 'yeeeaaaah' }));
+  app.use(everyauth.middleware());
+  everyauth.helpExpress(app)
 });
 
 app.configure('development', function(){
@@ -57,11 +51,6 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-
-app.get('/auth/twitter', passport.authenticate('twitter'));
-app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), function(req, res) {
-  res.redirect('/');
-});
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
