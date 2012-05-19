36253411adc1493a92c3f51859f89155347b5a1f
Brett Bukowski <brett.bukowski@gmail.com>
2012-05-19 14:18:28 -0600

    Refactoring

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  * Module dependencies.
  */
  var express = require('express')
+  , routes = require('./routes')
   , less = require('less-middleware')
   , everyauth = require('everyauth')
   , User = require('./models/user.js');
@@ -12,10 +13,10 @@
 everyauth.everymodule.userPkey('_id');
 everyauth.everymodule.findUserById(User.findById);
 
-var twitterCredentials = require('./config/twitter.js');
+var config = require('./config/keys.js');
 everyauth.twitter
-  .consumerKey(twitterCredentials.key)
-  .consumerSecret(twitterCredentials.secret)
+  .consumerKey(config.twitter.consumerKey)
+  .consumerSecret(config.twitter.consumerSecret)
   .findOrCreateUser(function(session, accessToken, accessTokenSecret, userInfo) {
     var promise = this.Promise();
     require('./models/user.js').findOrCreate(userInfo, accessToken, accessTokenSecret, promise);
@@ -30,7 +31,7 @@ var app = module.exports = express.createServer();
 app.configure(function() {
   app.use(express.bodyParser());
   app.use(express.cookieParser());
-  app.use(express.session({ secret: 'yeeeaaaah' }));
+  app.use(express.session({ secret: config.session }));
   app.use(everyauth.middleware());
   app.use(app.router);
   app.set('views', __dirname + '/views');
@@ -50,8 +51,8 @@ app.configure('production', function() {
 });
 
 // Routes
-app.get('/', require('./routes').index);
-app.post('/stash', require('./routes/stash.js').stash);
+app.get('/', routes.index);
+app.post('/stash', routes.stash);
 
 app.listen(3000, function() {
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,40 @@
 
 /*
- * GET home page.
+ * Route handlers
  */
- 
-exports.index = function(req, res){
-  if (req.user) {
-    res.render('user', { user: req.user, title: '' });
-  }
-  else {
-    res.render('index', { title: 'TweetStasher' });
+var Tweet = require('../models/tweet.js');
+
+module.exports = {
+  /*
+   * GET '/' home page.
+   */
+  index: function(req, res) {
+    if (req.user) {
+      res.render('user', { user: req.user, title: '' });
+    }
+    else {
+      res.render('index', { title: 'TweetStasher' });
+    }
+  },
+
+  /*
+   * GET '/' home page.
+   */
+  tweets: function(req, res) {
+    
+  },
+
+  /*
+   * POST '/stash' stash a new draft.
+   */
+  stash: function(req, res) {
+    if (!req.user) return res.header('401 Unauthorized');
+
+    var text = req.body.tweet;
+    if (text) {
+      Tweet.new({text: text}, req.user);
+    }
+
+    res.redirect('/');
   }
 };
