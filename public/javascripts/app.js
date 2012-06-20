e256950d49ed7c7c416dccec52d7a8dc19d071b2
Brett Bukowski <brett.bukowski@gmail.com>
2012-06-19 20:46:28 -0600

    Add a notify banner, cleanup relative dates, add comparator

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,32 +1,56 @@
 $(function() {
 
   var dateParts = [
-      { unit: 60, label: '%d minutes ago' }
-    , { unit: 60, label: '%d hours ago' }
-    , { unit: 24, label: '%d days ago' }
-    , { unit:  7, label: '%d weeks ago' }
-    , { unit:  4, label: '%d months ago' }
+      { unit: 60, label: '%d minute(s) ago' }
+    , { unit: 60, label: '%d hour(s) ago' }
+    , { unit: 24, label: '%d day(s) ago' }
+    , { unit:  7, label: '%d week(s) ago' }
+    , { unit:  4, label: '%d month(s) ago' }
   ];
 
+  // Makes a relative date out of the given timestamp or Date.
+  // Uses dateParts to construct a string.
   function relativeize(time) {
     if (_.isNumber(time)) {
       time = new Date(time);
     }
 
-    var diff = (new Date() - time) / 1000;
+    var diff = (new Date() - time) / 1000
+      , label
+      , relative;
 
     if (diff < 60) return 'moments ago';
 
-    var relative = _.find(dateParts, function(item, index, list) {
+    relative = _.find(dateParts, function(item, index, list) {
       diff /= item.unit;
       if (list[index + 1] && diff < list[index + 1].unit) {
         return true;
       }
     });
 
-    return (relative)
-      ? relative.label.replace('%d', Math.round(diff))
-      : time.toLocaleDateString();
+    if (relative) {
+      label = relative.label;
+      diff = Math.round(diff);
+
+      return label.replace('(s)', (diff === 1) ? '' : 's').replace('%d', diff);
+    }
+
+    return time.toLocaleDateString();
+  }
+
+  // Displays a notice with the given message.
+  // When clicked, the notice is removed, otherwise
+  // it removes itself after 9 seconds.
+  function notify(message) {
+    var note = $('<div id="notice" role="alert" aria-live="assertive" tabindex="0">' + message + '</div>').click(function() {
+      $(this).remove();
+    });
+    setTimeout(function() {
+      if (note && note.parent) {
+        note.remove();
+      }
+    }, 9000);
+    $(document.body).prepend(note).focus();
   }
 
   var Tweet = Backbone.Model.extend({
@@ -49,6 +73,9 @@ $(function() {
       }
     },
 
+    // Routes to '/instaPublish' or '/publish'
+    // depending on whether the tweet is new
+    // or has already been saved.
     publish: function(key, val, callback) {
       this.set(key, val);
       Backbone.sync.call(this, 'create', this, {
@@ -63,8 +90,14 @@ $(function() {
   });
 
   var Tweets = Backbone.Collection.extend({
-    url: '/tweets',
-    model: Tweet
+      url: '/tweets'
+    , model: Tweet
+    , comparator: function(a, b) {
+        a = a.get('created');
+        b = b.get('created');
+        if (a === b) return 0;
+        return (a < b) ? 1 : -1;
+      }
   });
 
   var stashed = new Tweets;
@@ -97,6 +130,9 @@ $(function() {
       return this;
     },
 
+    // Update the number of chars left.
+    // Subtracts out link length to Twitter's
+    // max t.co link length.
     updateCounter: function() {
       var text = this.input.val(),
           count = TweetView.charLimit - text.length;
@@ -124,6 +160,7 @@ $(function() {
       e.preventDefault();
       this.model.publish('text', this.input.val(), _.bind(function(model, response) {
         if (response.id) {
+          notify("<a href='https://twitter.com/" + response.user.screen_name + "/status/" + response.id_str + "' target='_blank'>Your tweet's been posted!</a>");
           this.model.destroy();
         }
       }, this));
@@ -139,13 +176,16 @@ $(function() {
     // www.
     pseudo: /(^|[^\/])(www\.[\S]+(\b|$))/gim,
     charLimit: 140,
+    // Current max t.co link length
     linkLength: 20
   });
 
   var AppView = Backbone.View.extend({
     el: $('#app'),
+
     initialize: function() {
       _.bindAll(this, 'add', 'addAll', 'ensureNewDraft');
+
       stashed.bind('add', this.add);
       stashed.bind('reset', this.addAll);
       stashed.bind('destroy', this.ensureNewDraft);
@@ -155,11 +195,13 @@ $(function() {
       // Get all the tweets!
       stashed.fetch({ add: true });
     },
+
     ensureNewDraft: function(model, collection) {
       if (model.isNew()) {
         this.addNewEmpty();
       }
     },
+
     addNewEmpty: function() {
       if (this.empty) {
         // Previous empty? Unsubscribe this listener
@@ -171,14 +213,17 @@ $(function() {
       this.empty.on('sync', this.addNewEmpty, this);
       this.add(this.empty, null, null, true);
     },
+
     add: function(model, index, all, prepend) {
       var view = new TweetView({ model: model });
       this.$el[(prepend) ? 'prepend' : 'append'](view.render().el);
       view.updateCounter();
     },
+
     addAll: function() {
       stashed.each(this.add);
     }
   });
+
   new AppView;
 });
